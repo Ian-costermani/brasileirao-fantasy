034beac998518f6840996131b94bda22046c14c6
Iuri07 <iuricostermani@hotmail.com>
2026-05-16 22:24:05 -0300

    fix(team-row): subs/pts com largura fixa pra alinhar entre rows

--- routes/mercado.tsx
+++ routes/mercado.tsx
@@ -193,7 +193,7 @@ export default function MercadoPage({ data }: PageProps<Data>) {
     <>
       <Head>
         <title>Mercado · Brasileirão Fantasy</title>
-        <link rel="stylesheet" href="/bf-styles.css?v=85" />
+        <link rel="stylesheet" href="/bf-styles.css?v=86" />
       </Head>
       <div class="bf-viewport">
         <TopBar
